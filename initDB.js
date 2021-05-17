0d13bf0a8cdc1ac2280451af8a36e75f1bc04efc
github_julienmcoding <manois.julien@gmail.com>
2021-05-17 16:04:18 +0200

    update

--- initDB.js
+++ initDB.js
@@ -1,8 +1,7 @@
 const { connect, connection } = require('mongoose');
-const { config } = require('dotenv'); 
-
-
-/*We'll use module.exports since we want to import this file in our server.js*/
+const { config } = require('dotenv');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 module.exports = () => {
  config(); //invoking the dotenv config here
@@ -22,19 +21,38 @@ module.exports = () => {
 
 connection.on('connected', () => {
     console.log('Mongoose connected to DB Cluster');
-})
+});
 
 connection.on('error', (error) => {
     console.error(error.message);
-})
+});
 
 connection.on('disconnected', () => {
     console.log('Mongoose Disconnected');
-})
+});
 
 process.on('SIGINT', () => {
     connection.close(() => {
         console.log('Mongoose connection closed on Application Timeout');
         process.exit(0);
-    })
-})
+    });
+});
+
+const enveloppeSchema = new Schema ({
+    id: {
+        type: Number,
+        unique: true
+    },
+    title: String,
+    budget: Number
+});
+
+const Enveloppe = mongoose.model('Enveloppe', enveloppeSchema);
+
+const createAndSaveEnveloppe = (done) => {
+  let julien = new Enveloppe({id: 5, title: 'Gifts', budget: 200});
+  julien.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
+};
