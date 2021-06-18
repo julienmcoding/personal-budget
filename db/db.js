d73c0375c99d1daf377aed1c2806499a3ef03ea3
github_julienmcoding <manois.julien@gmail.com>
2021-06-18 17:10:14 +0200

    cleaning code

--- db/db.js
+++ db/db.js
@@ -1,21 +1,6 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-/*const devConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE
-};
-
-const proConfig = {
-  connectionString: process.env.DATABASE_URL
-};
-
-const pool = new Pool(
-  process.env.NODE_ENV === 'production' ? proConfig : devConfig
-);*/
 const isProduction = process.env.NODE_ENV === 'production';
 
 const connectionString = 
